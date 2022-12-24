a71738cf2a4e4e28e69d6d509ab413b5319e38b5
mfarresalv <mfarresalv@gmail.com>
2022-12-24 17:08:21 +0100

    clean up code, now it will be easier to add new fields

--- changelog.md
+++ changelog.md
@@ -4,10 +4,10 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
-## 2022-12-14
+## [Unreleased]
 
 ### Added
-
+- cleaned up functions
 - added date type (start date, not periods)
 - Made date compatible with blanks (you can leave date columns in blank)
 
--- main.py
+++ main.py
@@ -38,42 +38,51 @@ def process_data(json:str)->pd.DataFrame:
     json: the raw data directly given by Notion API
     """
     results = json["results"]
-    plain_types = ["number","files","multi_select"]
-    array_types = ["formula","select","date"]
     list_ = []
 
     for row in results:
         fields = row["properties"]
         entry = {}
         for column in fields:
-            name = column
-            type = fields[column]["type"]
-            if type=="title":
-                title_type = fields[column][type][0]["type"]
-                name = "name"
-                value = fields[column][type][0][title_type]["content"]
-            if type in plain_types:
-                value = fields[column][type]
-            if type in array_types:
-                if type == "formula":
-                    sub_type = fields[column][type]["type"]
-                    if sub_type == "boolean":
-                        value = fields[column][type][sub_type]
-                    if sub_type == "date":
-                        value = fields[column][type][sub_type]["start"]
-                if type == "select":
-                    value = fields[column][type]["name"]
-                if type == "date":
-                    if fields[column][type]:
-                        value = fields[column][type]["start"]
-                    else:
-                        value = None
-            entry[name] = value
-            print(name, value)
+            fname, fvalue = process_datatypes(fields,column)
+            entry[fname] = fvalue
         list_.append(entry)
     df = pd.DataFrame(list_)
+    df["Fecha esperada"]=df["Fecha esperada"].astype("datetime64[ns]")
+    df["Fecha real"] = df["Fecha real"].astype("datetime64[ns]")
+    df["Next due"] = df["Next due"].astype("datetime64[ns]")
+    df["Fecha creación"] = df["Fecha creación"].astype("datetime64[ns]")
     return df
 
+def process_datatypes(json:str,column:str,more_entries=True):
+    """
+    returns each field and value idividually
+
+    """
+    name = column
+    ctype = json[column]["type"]
+    value = json[column][ctype]
+    if ctype == "title":
+        title_type = value[0]["type"]
+        name = "name"
+        value = value[0][title_type]["content"]
+        more_entries = False
+    elif type(value) != dict:
+        value = json[column][ctype]
+        more_entries = False
+    while more_entries:
+        if ctype=="select":
+            value = value["name"]
+        elif ctype=="date":
+            value=value["start"]
+        else:
+            ntype = value["type"]
+            value = value[ntype]
+        if type(value) != dict:
+            more_entries = False
+        else:
+            ctype=ntype
+    return name, value
 
 
 
